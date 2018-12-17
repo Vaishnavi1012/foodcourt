db397577fea59e907894fca5be84b31d5a5bc6c2
Vaishnavi Patel <vaishnavi@itobuz.com>
2018-12-17 19:10:34 +0530

    adding and deleting for particular table with total in localstorage done

--- app/menuListItem.js
+++ app/menuListItem.js
@@ -1,6 +1,6 @@
 
 export function menuListItem(item,addFoodList){
   return `
-  <li id="${item.id}" data-price="${item.price}" onclick="app.addFoodList('${item.id}','${item.name}','${item.price}')" ><img src="${item.menuimg}" height="64" width="64"><p>${item.name}</p></li>
+  <li id="${item.id}" onclick="app.addFoodList('${item.id}','${item.name}','${item.price}')" ><img src="${item.menuimg}" height="64" width="64"><p>${item.name}</p></li>
   `
   }
