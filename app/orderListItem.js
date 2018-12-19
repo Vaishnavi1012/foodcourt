148f862a4d12c86445b2ae3fb4bffb00beb16920
Vaishnavi Patel <vaishnavi@itobuz.com>
2018-12-19 13:12:53 +0530

    Designing with qty on orderlist done

--- app/orderListItem.js
+++ app/orderListItem.js
@@ -3,6 +3,12 @@ export function orderListItem(item,deleteOrderListItem){
 	var price = item.price*item.qty
 
 		return `
-  <li id="${item.id}" data-id="${item.itemId}" data-qty="${item.qty}"><span>${item.name}</span> <button onclick="app.deleteOrderListItem('${item.id}','${item.qty}')" >x</button><span> $ ${price}</span></li>
+  <li id="${item.id}" data-id="${item.itemId}" data-qty="${item.qty}">
+  <h3>${item.name}
+  x${item.qty}
+  <span class="order_cross" onclick="app.deleteOrderListItem('${item.id}','${item.qty}')"><img src="img/RedCross.jpg" width="16" /></span>
+  <span class="order_price">$${price}</span> </h3>
+  </li>
   `
   }
+
